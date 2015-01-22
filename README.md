6b246124fb9f1c6e1301f96732cbda38aa5253e2
mismayil <mismayil@uwaterloo.ca>
2015-01-22 16:15:15 -0500

    source files added

--- README.md
+++
@@ -1,2 +0,0 @@
-# UTrack
-An application to keep track of how your mental and physical health is affected by your activities
--- js/utrack.js
+++ js/utrack.js
@@ -127,31 +127,3 @@ window.addEventListener('load', function() {
         plotbar(activityModel.getActivityDataPoints());
     });
 });
-
-/**
- * This function can live outside the window load event handler, because it is
- * only called in response to a button click event
- */
-function runCanvasDemo() {
-    /*
-    Useful references:
-     http://www.w3schools.com/html/html5_canvas.asp
-     http://www.w3schools.com/tags/ref_canvas.asp
-     */
-    var canvas = document.getElementById('canvas_demo');
-    var context = canvas.getContext('2d');
-
-    var width = canvas.width;
-    var height = canvas.height;
-
-    console.log("Painting on canvas at: " + new Date());
-    console.log("Canvas size: " + width + "X" + height);
-
-    context.fillStyle = 'grey';
-    context.fillRect(0, 0, width, height);
-
-    context.strokeStyle = 'red';
-    context.moveTo(0, 0);
-    context.lineTo(width, height);
-    context.stroke();
-}
